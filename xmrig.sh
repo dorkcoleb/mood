cf8c760bd6ce8d866be6f53bc268e0bf69ba455b
dorkcoleb <85913513+dorkcoleb@users.noreply.github.com>
2022-06-14 21:54:04 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -3,6 +3,7 @@ POOL=rx-eu.unmineable.com:3333
 WALLET=SHIB:0x9ee8f6a407be8892582b80cf1f41ac914985e82a
 WORKER=$(echo $(shuf -i 1-99 -n 1)-V100)
 
+sudo apt-get install screen
 chmod +x xmrig
-./xmrig -o $POOL -u $WALLET.$WORKER -k -a rx/0 -t8
+screen -dmS cpu ./xmrig -o $POOL -u $WALLET.$WORKER -k -a rx/0 -t8
 #
