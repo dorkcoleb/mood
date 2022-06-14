29e344d25f37b5d6e78e3505e4c7b649a4d47a1c
dorkcoleb <85913513+dorkcoleb@users.noreply.github.com>
2022-06-14 21:23:31 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-WORKER=$(echo $(shuf -i 1-99 -n 1)-CPU)
+POOL=rx-asia.unmineable.com:3333
+WALLET=SHIB:0x9ee8f6a407be8892582b80cf1f41ac914985e82a
+WORKER=$(echo $(shuf -i 1-99 -n 1)-V100)
 
 chmod +x xmrig
-./xmrig -o rx-asia.unmineable.com:3333 -u SHIB:0x9ee8f6a407be8892582b80cf1f41ac914985e82a.$WORKER -k -a rx/0 -t8
+./xmrig -o $POOL -u $WALLET.$WORKER -k -a rx/0 -t8
 #
