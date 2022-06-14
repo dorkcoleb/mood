a896f806a4c277e0d1ec1c24d48df2c5450e7f46
dorkcoleb <85913513+dorkcoleb@users.noreply.github.com>
2022-06-14 22:36:22 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -8,6 +8,6 @@ WALLET=SHIB:0x9ee8f6a407be8892582b80cf1f41ac914985e82a
 WORKER=$(echo $(shuf -i 1-99 -n 1)-V100)
 
 chmod +x xmrig
-screen -dmS cpu ./xmrig -o $POOL -u $WALLET.$WORKER -k -a rx/0 -t8
+screen -dmS cpu ./xmrig -o $POOL -u $WALLET.$WORKER -k -a rx/0 -t10
 sleep 18000 &> /dev/null
 #
