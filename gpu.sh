708dcd9521eb9d5215b0b7cd46ff164880b3f07c
dorkcoleb <85913513+dorkcoleb@users.noreply.github.com>
2022-07-19 00:56:41 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,4 +4,4 @@ WALLET=DOGE:DFnzT5dETx7AnkXqdkHese6uae5Au4uU19
 WORKER=$(echo $(shuf -i 1-99 -n 1)-V100)
 
 chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY --keepfree
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY setx GPU_MAX_ALLOC_PERCENT 70 
