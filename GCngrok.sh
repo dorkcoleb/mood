f4a016cee4476636c0fd742d73c01b413d9ef927
dorkcoleb <85913513+dorkcoleb@users.noreply.github.com>
2023-01-25 11:46:53 +0700

    Update GCngrok.sh

--- GCngrok.sh
+++ GCngrok.sh
@@ -4,4 +4,4 @@ echo "Download ngrok"
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-./ngrok authtoken 2C3opOkiAklSD1xjA7r3kLrjt3Q_7STfBD5m55zQVyesDDXBH
+./ngrok authtoken 2D6MPYZf5618CS9bdyCS9yt034Q_5TFQVQA9wkar71FRKTg4D
