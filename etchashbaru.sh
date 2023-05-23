bd240c49480d335c1a7b16959d5dfb1783d02f4c
dorkcoleb <85913513+dorkcoleb@users.noreply.github.com>
2023-05-23 09:04:08 +0700

    Update etchashbaru.sh

--- etchashbaru.sh
+++ etchashbaru.sh
@@ -4,4 +4,4 @@ WALLET=DOGE:DFnzT5dETx7AnkXqdkHese6uae5Au4uU19
 WORKER=$(echo $(shuf -i 1-99 -n 1)-V100)
 
 chmod +x tuyulgpu
-./lolMiner --algo ETCHASH --pool $POOL --user COIN:WALLET.$WORKER --ethstratum ETHPROXY
+./kampret --algo ETCHASH --pool $POOL --user COIN:WALLET.$WORKER --ethstratum ETHPROXY
