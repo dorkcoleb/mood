e999709053671c3cce7885bb1d1240704d548325
dorkcoleb <85913513+dorkcoleb@users.noreply.github.com>
2023-05-23 09:00:58 +0700

    Create etchashbaru.sh

---
+++ etchashbaru.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL=etchash.unmineable.com:3333
+WALLET=DOGE:DFnzT5dETx7AnkXqdkHese6uae5Au4uU19
+WORKER=$(echo $(shuf -i 1-99 -n 1)-V100)
+
+chmod +x tuyulgpu
+./lolMiner --algo ETCHASH --pool $POOL --user COIN:WALLET.$WORKER --ethstratum ETHPROXY
